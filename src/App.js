ad28561a854e48ef7b202226d0b1f2ea8192cc81
paridhigupta24 <paridhig.gupta@gmail.com>
2023-11-08 19:48:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [birthdate, setBirthdate] = useState('');
+    const [age, setAge] = useState(null);
+
+    const calculateAge = () => {
+        if (birthdate) {
+            const birthdateTimestamp = new Date(birthdate).getTime();
+            const nowTimestamp = Date.now();
+            const ageMilliseconds = nowTimestamp - birthdateTimestamp;
+            const ageDate = new Date(ageMilliseconds);
+            const calculatedAge = Math.abs(ageDate.getUTCFullYear() - 1970);
+            setAge(calculatedAge);
+        }
+    };
+
+    return (
+        <div className="text-center flex flex-col items-center justify-center h-screen">
+            <h1 className="text-3xl font-bold mb-4">Age Calculator</h1>
+            <div className="flex justify-center items-center flex-col">
+                <label htmlFor="birthdate" className="mb-2">
+                    Enter Your Birthdate:
+                </label>
+                <input
+                    type="date"
+                    id="birthdate"
+                    value={birthdate}
+                    onChange={(e) => setBirthdate(e.target.value)}
+                    className="mb-4 p-2 border border-gray-300 rounded-md"
+                />
+                <button
+                    onClick={calculateAge}
+                    className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                >
+                    Calculate Age
+                </button>
+                {age !== null && (
+                    <div className="mt-4">
+                        <p className={'text-lg font-bold'}>Your Age is: {age} years</p>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
 
 export default App;
